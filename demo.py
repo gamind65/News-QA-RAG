cdc084499e5164047d069ddbefb18591d77c834e
gamind65 <tanpesi123@gmail.com>
2025-06-05 19:41:50 +0700

    init commit

---
+++ demo.py
@@ -0,0 +1,170 @@
+import ollama
+from langchain_ollama import OllamaEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain.prompts import ChatPromptTemplate, PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_community.vectorstores import FAISS
+from langchain_ollama.chat_models import ChatOllama
+from langchain_core.runnables import RunnablePassthrough
+from langchain.retrievers.multi_query import MultiQueryRetriever
+import pandas as pd
+import re
+import streamlit as st
+
+#---------------------
+# set up streamlit interface layout
+st.set_page_config(layout="wide")
+
+#---------------------
+# Load cache data
+@st.cache_resource()
+def load_cache_resource(llm_name="qwen3:1.7b", embed_name="nomic-embed-text"):
+    # Load LLM
+    llm = ChatOllama(model=llm_name, 
+                        enable_thinking=True, 
+                        Temperature=0.6, 
+                        TopP=0.95, TopK=20, 
+                        MinP=0) 
+    
+    # Load embedding model
+    embeddings = OllamaEmbeddings(model=embed_name)
+    return llm, embeddings
+
+llm, embeddings = load_cache_resource()
+
+#---------------------
+# Load resource to initialize compoments
+# Query prompt template
+QUERY_PROMPT = PromptTemplate(
+    input_variables=["question"],
+    template="""
+    Bạn là một trợ lý với nhiệm vụ giúp đỡ người dùng trả lời những câu hỏi liên quan đến bài báo.
+    Bạn sẽ thực hiện điều này bằng cách tạo ra thêm 2 câu hỏi khác tương đồng với câu hỏi gốc để 
+    tím kiếm những thông tin liên quan trong Vector Database.
+    Bằng cách tạo ra thêm những góc mới tương đồng với câu hỏi của người dùng, mục tiêu của bạn là 
+    giúp người dùng vượt qua rào cản của việc tìm kiếm thông tin dựa trên khoảng cách tương đối về 
+    mặt ngữ nghĩa của chúng. 
+    Hãy cố gắng để không trả lời những thông tin tồn tại trong bài báo.
+    Nếu bạn không biết câu trả lời, hãy cứ trả lời là không biết.
+    Hãy đưa ra câu trả lời trên một dòng mới và đừng lặp lại câu hỏi của người dùng. 
+    Hãy đưa ra câu trả lời một cách ngắn gọn.
+    Câu hỏi: {question}""",
+)
+
+# RAG prompt template
+template = """Hãy trả lời câu hỏi mà CHỈ dựa vào thông tin được cho như sau:
+{context}
+Câu hỏi: {question}"""
+
+#---------------------
+# Data chunking
+def data_chunking(article, size=512, overlap=0):
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=size, chunk_overlap=overlap)
+    chunks = text_splitter.split_text(text=article)
+    return chunks
+
+# Add created chunks into vector database 
+def create_vectordb(text_chunks):
+    vectordb = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
+    return vectordb
+
+def news_qa(question):
+    return 
+
+# Set up retriever
+def retriever(llm, vector_db, QUERY_PROMPT):
+    retriever_agent = MultiQueryRetriever.from_llm(
+        vector_db.as_retriever(), 
+        llm,
+        prompt=QUERY_PROMPT,
+    )
+    return retriever_agent
+
+# Data processing and generate answer
+def generate_ans(article, question):
+    chunks = data_chunking(article)                     # Create chunks  
+    vector_db = create_vectordb(chunks)                 # Create vector database for embedded chunks
+    retriever_agent = retriever(llm, vector_db, QUERY_PROMPT) # Create retriever
+    prompt = ChatPromptTemplate.from_template(template) # Create prompt template
+
+    # Create chain
+    chain = (
+        {"context": retriever_agent, "question": RunnablePassthrough()}
+        | prompt
+        | llm
+        | StrOutputParser()
+    )
+
+    # Generate answer
+    ans = re.sub('<think>(?s:.)*?</think>', '', chain.invoke(question))
+    return ans
+
+#---------------------
+def main():
+    # Default setup
+    # initiate 2 columns in streamlit UI
+    col1, col2 = st.columns([0.3, 0.7])
+    with col1:
+        # Title
+        st.subheader("Article section")
+
+    with col2:
+        # Title
+        st.header("Vietnamese news article QnA chatbot with RAG")
+        st.subheader("Chatbot section")
+
+        
+    
+    article = None
+    with col1:
+        # Textbox for article content
+        article = st.text_input("Article content", None, placeholder="Paste your news article content here...")
+        if article is not None:
+            st.write("Your article content:")
+            st.write(article)
+        
+    with col2:
+        # Initialize chat history
+        if "messages" not in st.session_state:
+            st.session_state.messages = []
+
+        # Display chat messages from history on app rerun
+        for message in st.session_state.messages:
+            with st.chat_message(message["role"]):
+                st.markdown(message["content"])
+
+        # Display user input
+        container = st.container()
+        if query := st.chat_input("What is up?"):
+            # Display user message in chat message container
+            with container:
+                with st.chat_message("user"):
+                    st.markdown(query)
+            # Add user message to chat history
+            st.session_state.messages.append({"role": "user", "content": query})
+
+            # Respond
+            if article is None: 
+                with container:
+                    with st.chat_message("assistant"):
+                        response = "Please add news article content in the left section first."
+                        st.markdown(response)
+                # Add assistant response to chat history
+                st.session_state.messages.append({"role": "assistant", "content": response})
+                
+            else:
+                response = generate_ans(article, query)
+                # Display assistant response in chat message container
+                with container:
+                    with st.chat_message("assistant"):
+                        st.markdown(response)
+                # Add assistant response to chat history
+                st.session_state.messages.append({"role": "assistant", "content": response})
+    return 
+
+if __name__ == '__main__':
+    main()
+
+
+
+
